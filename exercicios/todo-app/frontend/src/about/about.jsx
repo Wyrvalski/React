31b4e99832606d55f5a9463f20bcba67e2c28083
Eduardo Duarte <dudu_duarte@hotmail.com>
2020-02-04 20:46:37 -0300

    feat(teste): teste.

---
+++ exercicios/todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageheader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur...</p>
+        <h2>Missão e valor</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur...</p>
+    </div>
+)
